d4472d93a2fcb8068060f5de684c8a9aaa8c53a9
endersonmenezes <endersonster@gmail.com>
2026-05-11 19:55:07 +0000

    Deploying to gh-pages from @ codaqui/institucional@8b7eb13245b19ef509195b55aaeeb86713b16d38 🚀

---
+++ assets/js/2c44d5ee.6f5118d1.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkcodaqui_institucional=globalThis.webpackChunkcodaqui_institucional||[]).push([[237],{81063(i){i.exports=JSON.parse('{"authors":[{"name":"TI Social","title":"Equipe de Impacto","url":"https://www.instagram.com/tisocialmaringa","imageURL":"/img/tisocial.png","key":"tisocial","page":null,"count":10}]}')}}]);
